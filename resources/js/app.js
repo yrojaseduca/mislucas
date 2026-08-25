e01618337e3bcece81a1d4e669150affbf037cb5
yrojaseduca <yrojaseduca@users.noreply.github.com>
2026-08-25 14:25:47 +0200

    Keep interface readable in browser dark mode

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,4 +5,6 @@ import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import App from './App.vue';
-createApp(App).use(createPinia()).use(PrimeVue, { theme: { preset: Aura } }).mount('#app');
+createApp(App).use(createPinia()).use(PrimeVue, {
+    theme: { preset: Aura, options: { darkModeSelector: false } },
+}).mount('#app');
